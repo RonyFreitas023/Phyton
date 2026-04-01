135de7be93d54eb3c81c40f5a125252457bfc781
Rony <ronyfreitas.bg023@academico.ifsul.edu.br>
2026-04-01 01:47:25 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,7 @@
+nome= input ('Qual é seu nome?')
+
+dia= input('Dia =')
+mes= input('Mes =')
+ano= input('Ano =')
+
+print ('Ola',nome+'! Você nasceu dia', dia, 'de', mes, 'de', ano,'correto?' )
---
+++ aula2.py
@@ -0,0 +1,8 @@
+n1= int (input ('Numero1 ')) 
+n2= int (input ('Numero2 '))
+
+s= float((n1 + n2))
+
+#print('A soma entre',n1,'e', n2, 'é =', s)
+
+print ('A soma entre {} e {} é {}' .format (n1, n2, s))
